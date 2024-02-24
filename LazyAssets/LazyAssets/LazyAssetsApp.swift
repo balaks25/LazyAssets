d95967d7233ee6cb078307847f4d77bf5a492b67
Bala K S <60026841+balaks25@users.noreply.github.com>
2024-02-24 08:31:26 +0530

    initial setup

---
+++ LazyAssets/LazyAssets/LazyAssetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LazyAssetsApp.swift
+//  LazyAssets
+//
+//  Created by Balaji K S on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct LazyAssetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
